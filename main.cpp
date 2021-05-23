f68a7544498898fcc482474b7c84c79f7963d69f
guilhermesdl <>
2021-05-23 17:04:45 +0000

    comecei o codigo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <string.h>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int trocas(int *ponteiro,char a, char n, int *ponteiro2){
+int troca;
+
+
+return troca;
+}
+
+
+
 
 int main() {
-  std::cout << "Hello World!\n";
+int tamanho;
+char *palavra;
+string a;
+cout << "Digite uma palavra: ";
+cin >> a;
+
+palavra = &a[0];
+
+cout << palavra[1];
+
 }
